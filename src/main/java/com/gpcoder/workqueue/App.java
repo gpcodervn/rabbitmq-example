1583055c89bc82eba8393788787875769448c3fb
Phan Thanh Giang <ptgiang56it@gmail.com>
2020-04-30 00:19:40 +0700

    Default exchange with workers queue example

---
+++ src/main/java/com/gpcoder/workqueue/App.java
@@ -0,0 +1,16 @@
+package com.gpcoder.workqueue;
+
+public class App {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Producer producer1 = new Producer("[Producer 1]",10);
+        Consumer consumer1 = new Consumer("[Consumer 1]", 100);
+        Consumer consumer2 = new Consumer("[Consumer 2]", 300);
+
+        new Thread(producer1).start();
+
+        new Thread(consumer1).start();
+        new Thread(consumer2).start();
+    }
+}
