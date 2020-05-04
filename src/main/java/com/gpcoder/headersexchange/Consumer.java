f5921d8de3bc5665a93a60960d69dc08ed178109
Phan Thanh Giang <ptgiang56it@gmail.com>
2020-05-04 21:47:11 +0700

    Refactor code Headers Exchange

--- src/main/java/com/gpcoder/headersexchange/Consumer.java
+++ src/main/java/com/gpcoder/headersexchange/Consumer.java
@@ -3,21 +3,15 @@ package com.gpcoder.headersexchange;
 import com.rabbitmq.client.Connection;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
-import static com.gpcoder.headersexchange.Constant.EXCHANGE_NAME;
+import static com.gpcoder.headersexchange.Constant.*;
 
 public class Consumer {
 
-    private String queueName;
-    private Map<String, Object> headers;
     private HeadersExchangeChannel channel;
-
-    public Consumer(String queueName, Map<String, Object> headers) {
-        this.queueName = queueName;
-        this.headers = headers;
-    }
 
     public void start() throws IOException, TimeoutException {
         // Create connection
@@ -29,16 +23,37 @@ public class Consumer {
         // Create headers exchange
         channel.declareExchange();
 
+        // Create headers
+        Map<String, Object> devHeaders = new HashMap<>();
+        devHeaders.put("x-match", "any"); // Match any of the header
+        devHeaders.put("dev", "Developer Channel");
+        devHeaders.put("general", "General Channel");
+
+        Map<String, Object> managerHeaders = new HashMap<>();
+        managerHeaders.put("x-match", "any"); // Match any of the header
+        managerHeaders.put("dev", "Developer Channel");
+        managerHeaders.put("manager", "Manager Channel");
+        managerHeaders.put("general", "General Channel");
+
+        Map<String, Object> publishedHeaders = new HashMap<>();
+        publishedHeaders.put("x-match", "all"); // Match all of the header
+        publishedHeaders.put("general", "General Channel");
+        publishedHeaders.put("access", "publish");
+
         // Create queues
-        channel.declareQueues(queueName);
+        channel.declareQueues(DEV_QUEUE_NAME, MANAGER_QUEUE_NAME, PUBLISHED_QUEUE_NAME);
 
         // Binding queues with headers
-        channel.performQueueBinding(queueName, headers);
+        channel.performQueueBinding(DEV_QUEUE_NAME, devHeaders);
+        channel.performQueueBinding(MANAGER_QUEUE_NAME, managerHeaders);
+        channel.performQueueBinding(PUBLISHED_QUEUE_NAME, publishedHeaders);
     }
 
     public void subscribe() throws IOException {
         // Subscribe message
-        channel.subscribeMessage(queueName);
+        channel.subscribeMessage(DEV_QUEUE_NAME);
+        channel.subscribeMessage(MANAGER_QUEUE_NAME);
+        channel.subscribeMessage(PUBLISHED_QUEUE_NAME);
     }
 
 }
--- src/main/java/com/gpcoder/headersexchange/HeadersExchangeChannel.java
+++ src/main/java/com/gpcoder/headersexchange/HeadersExchangeChannel.java
@@ -7,7 +7,6 @@ import com.rabbitmq.client.Connection;
 
 import java.io.IOException;
 import java.util.Map;
-import java.util.concurrent.TimeoutException;
 
 public class HeadersExchangeChannel {
 
@@ -43,9 +42,6 @@ public class HeadersExchangeChannel {
         channel.basicConsume(queueName, true, ((consumerTag, message) -> {
             System.out.println("[Received] [" + queueName + "]: " + consumerTag);
             System.out.println("[Received] [" + queueName + "]: " + new String(message.getBody()));
-
-            // Release right after received one message
-            // release();
         }), consumerTag -> {
             System.out.println(consumerTag);
         });
@@ -58,17 +54,5 @@ public class HeadersExchangeChannel {
         // basicPublish - ( exchange, routingKey, basicProperties, body)
         System.out.println("[Send] [" + headers + "]: " + message);
         channel.basicPublish(exchangeName, "", properties, message.getBytes());
-
-        // Release right after published one message
-        // release();
-    }
-
-    public void release() {
-        try {
-            channel.close();
-            connection.close();
-        } catch (IOException | TimeoutException e) {
-            e.printStackTrace();
-        }
     }
 }
